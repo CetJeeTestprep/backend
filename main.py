7a0c16f94a42867f741e2e9e65c01f979519c9bf
millonigada <milloni.gada251@gmail.com>
2022-10-30 01:22:56 +0530

    BASE API

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse, abort
+#from requests import request
+
+app = Flask(__name__)
+api = Api(app)
+
+classname_post_args = reqparse.RequestParser()
+classname_post_args.add_argument("name", type=str, help="Name of the exam is required", required=True)
+classname_post_args.add_argument("type", type=str, help="Type of the exam")
+classname_post_args.add_argument("year", type=int, help="Year of the exam")
+
+names = {"cet": {"type": "offline", "year": 2022}, 
+         "jee": {"type": "online", "year": 2023}}
+
+def abort_if_name_doesnt_exist(name):
+    if name not in names:
+        abort(404, message = "Could not find name, try again...")
+
+class HelloWorld(Resource):
+    def get(self, name):
+        abort_if_name_doesnt_exist(name)
+        return names[name]
+
+    def post(self, name):
+        if name in names:
+            abort(409, message = "Name already exists")
+        args = classname_post_args.parse_args()
+        names[name] = args
+        return args, 202
+
+    def put(self, name):
+        args = classname_post_args.parse_args()
+        names[name] = args
+        return names[name], 201
+
+    def delete(self, name):
+        abort_if_name_doesnt_exist(name)
+        del names[name]
+        return '', 204
+
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
