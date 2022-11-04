aecadeb5d409d8364d1984afaae6a9b3834f2bf5
millonigada <milloni.gada251@gmail.com>
2022-11-04 16:08:18 +0530

    structure

---
+++ services/result.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse, abort
+import requests
+import constants.urls as urls
+#from requests import request
+
+BASE = urls.BASE
+class ResultServices(Resource):
+
+    user_post_args = reqparse.RequestParser()
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def get(self):
+        return 200
+
+    def post(self):
+        return 202
+
+    def put(self):
+        return 201
+
+    def delete(self):
+        return '', 204
